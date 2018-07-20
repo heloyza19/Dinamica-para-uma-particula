62aa931f8d0fe0ffbfe3f92530ea3cd70635eb13
Heloyza Gois <36577707+heloyza19@users.noreply.github.com>
2018-07-19 23:20:17 -0300

    Add files via upload

--- dados_principal.m
+++ dados_principal.m
@@ -1,7 +1,10 @@
 clear all; clc;
 close all;
-na=input('número de vértices:');         %número de arestas
-[pt,x,y]=poligono(na);                   %pt matriz com as coordenadas
+%na=input('número de vértices:');         %número de arestas
+pt=[1 1; 1 2; 2 2; 2 1; 1.5 1.5];
+x=[1 1 2 2 1.5];
+y=[1 2 2 1 1.5];
+%[pt,x,y]=poligono(na);                   %pt matriz com as coordenadas
 [XC,YC,I,m]=centromassa(pt,x,y);
 dinamica(x,y,XC,YC,I,m);
 
--- dinamica.m
+++ dinamica.m
@@ -37,16 +37,17 @@ dt = 0.01;             % Intervalo de tempo (s)
 times = 0 : dt : t;    %calcular tempo final da queda
 
 %figure
-ymin=ordem_y(1);
+ymax=ordem_y(1);
 for i = 1:length(times)
+    %h =20+ordem_y(np); 
     
     % Representação das partículas e caixa 
-    plot(X,Y); fill(X,Y,'b'); hold on;
+    plot(X,Y); fill(X,Y,'r'); hold on;
     plot([0 d],[0 0],'k',[0 d],[h h],'w',[0 0],[0 h],'w',[d d],[0 h],'w','LineWidth',5); hold off;
     axis([-0.1,d+0.1,-0.1,h+0.1]); 
     xlabel('Coordenada x (m)');
     ylabel('Coordenada y (m)');
-     if ymin==0
+     if ymax==0
          break
      end
     
@@ -64,19 +65,30 @@ for i = 1:length(times)
     Vy=Vy+Ay*dt;
     
     ordem_y=sort(Y);
-    ymin=ordem_y(1);
+    ordem_x=sort(X);
+    ymax=ordem_y(np);
+    xmax=ordem_x(np);
     
     %Rotação do objeto
     X=ones(1,np).*Xc+R.*cos(teta);
     Y=ones(1,np).*Yc+R.*sin(teta);
-    teta=teta+ones(1,np)*W*dt;
+    c1=W;
+    c2=W+alfa*dt;
+    teta=teta+ones(1,np)*0.5*(c1+c2);
     W=W+alfa*dt; 
     %O objeto deve parar ao chegar em y=0 (chão)
-    %if ymin<0
+    if ymax>h || xmax>d
         %deltay=ymin;
         %Y=Y+ones(1,np)*deltay;
         %ymin=0;
-    %end
+        %Xc
+        %Ax
+        %Yc
+        %Ay
+        %m
+        %times(i)
+        break
+    end
     
     % Grava o plot atual
     mov(i)=getframe(gcf);
